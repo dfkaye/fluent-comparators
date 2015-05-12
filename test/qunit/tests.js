0608b5e18292f1663ad5ab27115ac25a57f1f2e6
david kaye <dfkaye@yahoo.com>
2015-05-12 15:51:42 -0700

    Update tests.js

--- test/qunit/tests.js
+++ test/qunit/tests.js
@@ -1,5 +1,4 @@
 
-
 QUnit.module('eq');
 
 QUnit.test( "number.eq(number)", function( assert ) {
@@ -8,62 +7,55 @@ QUnit.test( "number.eq(number)", function( assert ) {
 });
 
 QUnit.test( "string.eq(string)", function( assert ) {
-
   assert.ok( ('text').eq('text'), "('text').eq('text')!" );
   assert.ok( !('text').eq('asdf'), "!('text').eq('asdf')!" );
 });
 
 QUnit.test( "boolean.eq(boolean)", function( assert ) {
-
   assert.ok( (false).eq(false), "(false).eq(false)!" );
   assert.ok( (true).eq(true), "(true).eq(true)!" );
   assert.ok( !(false).eq(true), "!(false).eq(true)!" );
 });
 
 QUnit.test( "x.eq(null || |undefined)", function( assert ) {
-
   assert.ok( (typeof {}.eq).eq('undefined'), "object.eq!" );
   assert.ok( (typeof null).eq('object'), "(typeof null).eq('object')!" );
   assert.ok( (typeof undefined).eq('undefined'), "(typeof undefined).eq('undefined')!" );
 });
 
+
 QUnit.module('gt');
 
 QUnit.test( "number.gt(number)", function( assert ) {
-
   assert.ok( (2.0).gt(1.999999999999999), "(2.0).gt(1.999999999999999)" );
   assert.ok( !(1.999999999999999).gt(2.0), "!(1.999999999999999).gt(2.0)!" );
 });
 
 QUnit.test( "string.gt(string)", function( assert ) {
-
-	assert.ok( ('texta').gt('text'), "('texta').gt('text')!" );
-	assert.ok( !('text').gt('text'), "!('text').gt('text')!" );
+  assert.ok( ('texta').gt('text'), "('texta').gt('text')!" );
+  assert.ok( !('text').gt('text'), "!('text').gt('text')!" );
 });
   
 QUnit.test( "boolean.gt(boolean)", function( assert ) {
-
 	assert.ok( !(false).gt(true), "!(false).gt(true)!" );
 	assert.ok( (true).gt(false), "(true).gt(false)!" );
 });
 
+
 QUnit.module('gte');
 
 QUnit.test( "number.gte(number)", function( assert ) {
-
 	assert.ok( (-2).gte(-3), "(-2).gte(-3)!" );
 	assert.ok( !(-3).gte(-2), "!(-3).gte(-2)!" );
 	assert.ok( !(NaN).gte(NaN), "!(NaN).gte(NaN)!");
 });
 
 QUnit.test( "string.gte(string)", function( assert ) {
-
 	assert.ok( ('texta').gte('text'), "('texta').gte('text')!" );
 	assert.ok( ('text').gte('text'), "('text').gte('text')!" );
 });
 
 QUnit.test( "boolean.gte(boolean)", function( assert ) {
-	
 	assert.ok( !(false).gte(true), "!(false).gte(true)!" );
 	assert.ok( (true).gte(false), "(true).gte(false)!" );
 });
@@ -71,7 +63,6 @@ QUnit.test( "boolean.gte(boolean)", function( assert ) {
 QUnit.module('lt');
 
 QUnit.test( "number.lt(number)", function( assert ) {
-
 	assert.ok( (2).lt(3), "(2).lt(3)!" );
 	assert.ok( !(3).lt(2), "!(3).lt(2)!" );
 });
@@ -83,28 +74,25 @@ QUnit.test( "string.lt(string)", function( assert ) {
 });
 
 QUnit.test( "boolean.lt(boolean)", function( assert ) {
-	
 	assert.ok( (false).lt(true), "(false).lt(true)!" );
 	assert.ok( !(true).lt(false), "!(true).lt(false)!" );
 });
 
+
 QUnit.module('lte');
 
 QUnit.test( "number.lte(number)", function( assert ) {
-	
 	assert.ok( (-1).lte(-0.5), "(-1).lte(-0.5)!" );
 	assert.ok( !(-0.5).lte(-1), "!(-0.5).lte(-1)!" );
 	assert.ok( !(NaN).lte(NaN), "!(NaN).lte(NaN)!");
 });
 
 QUnit.test( "string.lte(string)", function( assert ) {
-	
 	assert.ok( ('text').lte('texty'), "('text').lte('texty'))!" );
 	assert.ok( !('text').lte('atext'), "!('text').lte('atext')!" );
 });
 
 QUnit.test( "boolean.lte(boolean)", function( assert ) {
-	
 	assert.ok( (false).lte(true), "(false).lte(true)!" );
 	assert.ok( !(true).lte(false), "!(true).lte(false)!" );
 });
@@ -140,10 +128,12 @@ QUnit.test( "object properties", function( assert ) {
 	assert.ok( test.boolean.eq(true), "test.boolean.eq(true)!" );
 	assert.ok( test.string.eq('string'), "test.string.eq('string')!" );
 	assert.ok( test.string.eq(String('string')), "test.string.eq(String('string'))!" );
+	
 	// neq
 	assert.ok( !test.number.eq('1'), "!test.number.eq('1')!" );
 	assert.ok( !test.boolean.eq('true'), "!test.boolean.eq('true')!" );
 	assert.ok( !test.string.eq(new String('string')), "!test.string.eq(new String('string'))!" );
+	
 	assert.ok( test[''].eq(''), "test[''].eq('')!" );
 });
 
