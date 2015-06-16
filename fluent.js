df3683ee8b081f31c42f4aaeb4c67521d3db6897
david kaye <dfkaye@yahoo.com>
2015-06-16 15:59:46 -0700

    add mod() support

--- fluent.js
+++ fluent.js
@@ -8,10 +8,11 @@
 // + changed from Object.prototype, to Number, String and Boolean prototypes
 // + moved to repository with tests at https://github.com/dfkaye/fluent-comparators
 
-// 13-May-2015:
-// + added `neq`
+// 16-June-2015:
+// + add mod() support
 
 // TODO 10-May-2015:
+// + add `not` or `neq` ??
 // + add callback support:
 //
 //      value.eq(other, function capture(result) {
@@ -20,13 +21,14 @@
 //      });
 
 (function() {
+  
   var comp = {
         'eq': function eq(n) { return this.valueOf() === n; },
         'gt': function gt(n) { return this > n; },
         'gte': function gte(n) { return this >= n; },
         'lt': function lt(n) { return this < n; },
         'lte': function lte(n) { return this <= n; },
-        'neq': function neq(n) { return !this.eq(n); } 
+        'mod': function mod(n) { return this % n; }
       },
       b = Boolean.prototype,
       n = Number.prototype,
@@ -39,4 +41,5 @@
     k in n || (n[k] = f);
     k in s || (s[k] = f);
   }
+  
 }());
